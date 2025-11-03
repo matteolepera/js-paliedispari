ac4dbd438a471916bbd560039ef6fb5170767852
matteolpera <matteo.lepera02@gmail.com>
2025-11-03 16:05:13 +0100

    pali completat

--- palindroma/script.js
+++ palindroma/script.js
@@ -10,3 +10,30 @@
 // come confronto mi basta arrivare al centro, tipo:
 // a e r e a, mi basta avere il confronto con 0 e 4, 1 e 3, se è true allora la mia parola è palindroma.
 
+
+function isPalindrome(word) {
+    word = word.toLowerCase();
+    let lengthWordUser = word.length;
+    let checkPalindrome = true;
+
+    for (let i = 0; i < lengthWordUser / 2; i++) {
+        // console.log(i);
+        if (word[i] !== word[lengthWordUser - 1 - i]) {
+            checkPalindrome = false;
+            break;
+        }
+    }
+
+    return checkPalindrome;
+}
+
+let wordUser = prompt("inserisci una parola!");
+// console.log(wordUser);
+
+let result = "non è palindroma!";
+
+if (isPalindrome(wordUser)) {
+    result = "è palindroma!";
+}
+
+console.log(`la parola ${wordUser} ${result}`);
