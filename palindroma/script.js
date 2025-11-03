2fbbc272c3b7d029dab252c65454027903dcd55b
matteolpera <matteo.lepera02@gmail.com>
2025-11-03 15:12:03 +0100

    first commit

---
+++ palindroma/script.js
@@ -0,0 +1,12 @@
+// Chiedere all’utente di inserire una parola
+// Creare una funzione per capire se la parola inserita è palindroma (NON usare split, reverse e join)
+//
+//RAGIONAMENTO
+// creo una variabile con una parola inserita dall'utent (prompt).
+// come faccio a verificare che la parola è palindroma?
+// in ingresso avrò sicuramente una stringa, dovrei considerare sicuramente le maiuscole e minuscole.
+// ogni stringa ha come indice iniziale lo zero, magari con .length prendo la lunghezza e questo valore
+// lo posso usare come confronto?
+// come confronto mi basta arrivare al centro, tipo:
+// a e r e a, mi basta avere il confronto con 0 e 4, 1 e 3, se è true allora la mia parola è palindroma.
+
